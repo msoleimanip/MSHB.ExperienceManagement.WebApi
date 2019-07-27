0c3413f12234c442e4870b790890128c41399f89
Mohammad Soleimani <m.soleimani.p@gmail.com>
2019-07-27 10:07:16 +0430

    init

--- src/Layers/MSHB.ExperienceManagement.Layers.L03_Services/Impls/ReportService.cs
+++ src/Layers/MSHB.ExperienceManagement.Layers.L03_Services/Impls/ReportService.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using MSHB.ExperienceManagement.Layers.L00_BaseModels.Constants.Messages.Base;
 using MSHB.ExperienceManagement.Shared.Common.GuardToolkit;
 using MSHB.ExperienceManagement.Layers.L01_Entities.Models;
+using System.Linq;
 
 namespace MSHB.ExperienceManagement.Layers.L03_Services.Impls
 {
@@ -88,36 +89,30 @@ namespace MSHB.ExperienceManagement.Layers.L03_Services.Impls
             }
         }
 
-        public Task<List<IssueOfUsersViewModel>> IssueOfUsersReportAsync(User user, IssueOfUsersFormModel form)
+        public async Task<List<IssueOfUsersViewModel>> IssueOfUsersReportAsync(User user, IssueOfUsersFormModel form)
         {
             try
             {
-                //var resp = await _context.ReportStructures.FirstOrDefaultAsync(c => c.Id == form.ReportStructureModelId);
-                //if (resp is null && form.ReportStructureModelId != 0)
-                //{
-                //    throw new ExperienceManagementGlobalException(ReportServiceErrors.ReportStructureNotFoundError);
-                //}
-                //if (resp != null)
-                //{
-                //    resp.Configuration = form.Configuration;
-                //    resp.LastUpdatedDateTime = DateTime.Now;
-                //    resp.ReportId = form.ReportId;
-                //    _context.ReportStructures.Update(resp);
-                //}
-                //else
-                //{
-                //    var report = new ReportStructure()
-                //    {
-                //        Configuration = form.Configuration,
-                //        CreationDate = DateTime.Now,
-                //        LastUpdatedDateTime = DateTime.Now,
-                //        ReportId = form.ReportId
-                //    };
-                //    await _context.ReportStructures.AddAsync(report);
-                //}
-                //await _context.SaveChangesAsync();
-                //return true;
-                throw new ExperienceManagementGlobalException(ReportServiceErrors.IssueOfUsersReportError);
+                var resp = await _context.Users.Include(c=>c.Organization).Include(c=>c.Issues).Include(c=>c.IssueDetails).Where(c => form.Users.Contains(c.Id)).ToListAsync();
+
+                var issueOfUsersViewModels = new List<IssueOfUsersViewModel>();
+                resp?.ToList().ForEach(async c =>
+                {
+                    var issueEq = await _context.EquipmentIssueSubscriptions.Include(f=>f.Equipment).Where(d => c.Issues.Contains(d.Issue)).Select(e => e.Equipment.EquipmentName).ToListAsync();  
+                    var issueOfUsersViewModel = new IssueOfUsersViewModel()
+                    {
+                        FullName = c.FirstName + " " + c.LastName,
+                        OrganizationName = c.Organization.OrganizationName,
+                        TotalIssueCount = c.Issues.Count,
+                        TotalIssueUserDetails=c.IssueDetails.Count,
+                        TotalIssueEquipment=string.Join(",",issueEq)
+                    };
+                    issueOfUsersViewModels.Add(issueOfUsersViewModel);
+
+                });
+
+
+                return issueOfUsersViewModels;
 
             }
             catch (Exception ex)
--- src/Layers/MSHB.ExperienceManagement.Layers.L04_ViewModels/ViewModels/IssueOfUsersViewModel.cs
+++ src/Layers/MSHB.ExperienceManagement.Layers.L04_ViewModels/ViewModels/IssueOfUsersViewModel.cs
@@ -6,5 +6,10 @@ namespace MSHB.ExperienceManagement.Layers.L04_ViewModels.ViewModels
 {
     public class IssueOfUsersViewModel
     {
+        public long TotalIssueCount { get; set; }
+        public string TotalIssueEquipment { get; set; }
+        public long TotalIssueUserDetails { get; set; }
+        public string OrganizationName { get; set; }
+        public string FullName { get; set; }
     }
 }
