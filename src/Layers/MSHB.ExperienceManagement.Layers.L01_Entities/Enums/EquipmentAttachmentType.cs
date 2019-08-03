84c9a00ba8f2dac15bdcf78c7e652af3247cf52a
hamedbagheri1992 <bagheri.h224@gmail.com>
2019-08-03 15:24:53 +0430

    init

--- src/Layers/MSHB.ExperienceManagement.Layers.L01_Entities/Enums/EquipmentAttachmentType.cs
+++ src/Layers/MSHB.ExperienceManagement.Layers.L01_Entities/Enums/EquipmentAttachmentType.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MSHB.ExperienceManagement.Layers.L01_Entities.Enums
 {
     public enum EquipmentAttachmentType
     {
+        [Display(Name = "اسناد")]
         Document = 1,
-        Tool,
-        Position,
+
+        [Display(Name = "ابزار")]
+        Tool = 2,
+
+        [Display(Name = "مکان")]
+        Position = 3,
+
+        [Display(Name = "کاتالوگ")]
+        Catalog = 4,
+
+        [Display(Name = "مواد")]
+        Materials = 5,
+
+        [Display(Name = "لوازم جانبی")]
+        Accessory = 6
     }
 }
